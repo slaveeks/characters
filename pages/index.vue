ceee728097684d7750ff8e28db6247fb740bb59d
slaveeks <chernyshev2001@icloud.com>
2023-08-02 18:25:57 +0300

    Added base for nuxt project with index page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLink to="/characters">Characters</NuxtLink>
+  </div>
+  <div>
+    <NuxtLink to="/locations">Locations</NuxtLink>
+  </div>
+  <div>
+    <NuxtLink to="/episodes">Episodes</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
