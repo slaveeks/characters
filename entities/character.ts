4de0b53bc2263f6380f08a4371877845ddd08ba9
slaveeks <chernyshev2001@icloud.com>
2023-08-03 14:33:51 +0300

    Added base for Transport and added Character etity interface

---
+++ entities/character.ts
@@ -0,0 +1,38 @@
+/**
+ * Character status enum
+ */
+export enum Status {
+    Alive = 'Alive',
+    Dead = 'Dead',
+    Unknown = 'unknown'
+}
+
+/**
+ * Character interface
+ */
+export default interface Character {
+    /**
+     * Character identifier to be used as key
+     */
+    id: number;
+
+    /**
+     * Character name
+     */
+    name: string;
+
+    /**
+     * Character status, can be Alive, Dead or unknown
+     */
+    status: Status;
+
+    /**
+     * Character image URL
+     */
+    image: string;
+
+    /**
+     * Character episode list, each episode is a url
+     */
+    episode: string[];
+}
